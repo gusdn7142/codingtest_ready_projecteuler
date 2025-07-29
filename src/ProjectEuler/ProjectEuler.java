60a2753dc174a3464989959b8641c3c708e4db66
gusdn7142 <gusdnchl7144@naver.com>
2025-07-29 20:30:07 +0900

    feat: 프로젝트 오일러 1번 문제 해결
    배수의 법칙 역이용 (3*4=12 --> 12%3=0)

---
+++ src/ProjectEuler/ProjectEuler.java
@@ -0,0 +1,28 @@
+package ProjectEuler;
+
+import java.util.Scanner;
+
+/**
+ * 출처 : https://euler.synap.co.kr/problem=1
+ * 문제1 : 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면?
+ * 상세설명
+ * - 10보다 작은 자연수 중에서 3 또는 5의 배수는 3, 5, 6, 9 이고, 이것을 모두 더하면 23입니다.
+ * - 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마일까요?
+ * 풀이방식
+ * - 배수의 법칙 역이용 (3*4=12 --> 12%3=0)
+ */
+public class ProjectEuler {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        for(int naturalNum = 1; naturalNum < 1000; naturalNum++) {
+            if(naturalNum % 3 == 0 || naturalNum % 5 ==0){
+                sum += naturalNum;
+            }
+        }
+        
+        System.out.println(sum);
+    }
+}
